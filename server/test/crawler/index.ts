256a1ba2437e604c176fbb9abb6d07af51ab5f65
Mirai3103 <huuhoag1412@gmail.com>
2023-03-16 11:06:38 +0700

    init project

---
+++ server/test/crawler/index.ts
@@ -0,0 +1,7 @@
+export {};
+// https://kakarot.cuutruyen.net/api/v2/mangas/recently_updated?page=1&per_page=30
+// https://kakarot.cuutruyen.net/api/v2/mangas/673
+// https://kakarot.cuutruyen.net/api/v2/mangas/673/chapters
+// https://kakarot.cuutruyen.net/api/v2/chapters/15798
+// https://comick.app/comic/spy-family
+// kanojo-okarishimasu
